497ca6e271ac18ae8562fb1effb8d70d189f4292
Wlllysse Melo <wllyssesmelo2016@gmail.com>
2024-09-07 11:06:03 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,46 +1,53 @@
 let dados = [
     {  
+        titulo : "Física",
+        descricao : "A Física é a ciência natural que estuda os fenômenos da natureza, buscando compreender as leis que regem o universo. Ela investiga desde as partículas subatômicas até os corpos celestes, passando por fenômenos como a luz, o som, o movimento e a energia.Em resumo, a Física busca responder perguntas como:Por que os objetos caem?Como a luz se propaga?O que é a matéria?Quais são as leis que governam o universo?Para aprofundar seus conhecimentos, recomendo este Mecânica: Estuda o movimento dos corpos.Termologia: Estuda os fenômenos relacionados à temperatura e ao calor.Óptica: Estuda os fenômenos relacionados com a luz.Eletricidade: Estuda os fenômenos elétricos e magnéticos.Física Moderna: Aborda temas como a teoria da relatividade e a mecânica quântica.",
+        link : "https://brasilescola.uol.com.br/o-que-e/fisica",
+        tags:"fisica,ciencia natural, eletricidade, moderna ",
+   
+ },
+    {  
         titulo : "Fundamentos da Radioastronomia",
         descricao : "A radioastronomia é um ramo da astronomia que se dedica ao estudo do universo através das ondas de rádio emitidas ou refletidas por objetos celestes. Ao contrário da astronomia óptica, que utiliza telescópios para captar a luz visível, a radioastronomia emprega radiotelescópios para detectar essas ondas de rádio, que são invisíveis a olho nu",
         link : "https://www.radioastronomia.pro.br/",
-        tags:"radio ,telescopio ,visivel ,optica ,atraves ,celeste"
+        tags:"radio ,telescopio ,visivel ,optica ,atraves ,celeste",
    
    },
    {  
         titulo : "Como funciona a radioastronomia?",
         descricao : "Radiotelescópios: Esses instrumentos gigantescos, muitas vezes compostos por antenas parabólicas, captam as ondas de rádio provenientes do espaço, Processamento de dados: Os sinais captados pelos radiotelescópios são convertidos em dados que são analisados por computadores poderosos, Imagens: A partir desses dados, os astrônomos podem criar imagens detalhadas dos objetos celestes, revelando suas estruturas e características.",
         link : "https://www.radioastronomia.pro.br/",
-        tags:"radio, sinais, sanal, parabolica, parabolicas"
+        tags:"radio, sinais, sanal, parabolica, parabolicas",
     },
     {  
         titulo : "Descobertas importantes da radioastronomia?",
         descricao : "Quasares: Objetos extremamente distantes e brilhantes, que emitem grandes quantidades de energia,        Pulsares: Estrelas de nêutrons que giram rapidamente e emitem pulsos de radiação, Masers: Análogos cósmicos dos lasers, que amplificam a radiação na faixa de micro-ondas, Radiação cósmica de fundo: Evidência crucial para a teoria do Big Bang",
         link : "https://www.radioastronomia.pro.br/",
-        tags:"cosmicos, micro onda, micro-ondas, radiaçao, cosmico"
+        tags:"cosmicos, micro onda, micro-ondas, radiaçao, cosmico",
     },
     {
         titulo : "Karl Jansky",
         descricao : "Karl Jansky foi um engenheiro e físico norte-americano que, sem querer, revolucionou a astronomia. Em 1931, enquanto trabalhava na Bell Telephone Laboratories, Jansky estava investigando fontes de interferência em transmissões de rádio de ondas curtas",
         link : "https://pt.wikipedia.org/wiki/Karl_Guthe_Jansky",
-        tags:"1931, fisico, janski, carl,telefone "
+        tags:"1931, fisico, janski, carl,telefone ",
     },
     {
         titulo : "Grote Reber",
         descricao : "Pioneiro da radioastronomia que construiu o primeiro radiotelescópio direcional em seu quintal e criou o primeiro mapa de rádio do céu.",
         link : "https://en.wikipedia.org/wiki/Grote_Reber",
-        tags:"ceu"
+        tags:"ceu",
     },
     {
         titulo : "Jocelyn Bell Burnell",
         descricao : "Astrônoma irlandesa que, como estudante de pós-graduação, descobriu os pulsares, estrelas de nêutrons que emitem radiação eletromagnética em pulsos regulares.",
         link : "https://en.wikipedia.org/wiki/Jocelyn_Bell_Burnell",
-        tags:"eletromagnetico, eletromagnetica, astronomo"
+        tags:"eletromagnetico, eletromagnetica, astronomo",
     },
     {
         titulo : "Antony Hewish",
         descricao : "Astrônomo britânico que, junto com Jocelyn Bell Burnell, recebeu o Prêmio Nobel de Física pela descoberta dos pulsares. Seus trabalhos contribuíram significativamente para o desenvolvimento da radioastronomia.",
         link : "https://en.wikipedia.org/wiki/Antony_Hewish",
-        tags:"premio, antoni "
+        tags:"premio, antoni ",
     },
    
 
